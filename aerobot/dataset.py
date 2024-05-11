Fixing error in normalization

aerobot/dataset.py
@@ -47,7 +47,8 @@ def dataset_normalize(dataset:Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame
     :param dataset
     :return: A dataset with constant-sum normalized rows. 
     '''
-    dataset['features'] = dataset['features'].apply(lambda row : row / row.sum(), axis=0)
+    dataset['features'] = dataset['features'].apply(lambda row : row / row.sum(), axis=1)
+    #assert np.all(dataset['features'].values.sum(axis=1) == 1), 'dataset_normalize: Normalization failed.'
     return dataset
 
 

scripts/train.py
@@ -28,11 +28,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('model-class', choices=['nonlinear', 'logistic'], help='The type of model to train.')
-    parser.add_argument('--feature-type', '-f', type=str, default=None, choices=FEATURE_SUBTYPES + FEATURE_TYPES, help='The feature type on which to train.')
+    parser.add_argument('--feature-type', '-f', type=str, default=None, nargs='+', choices=FEATURE_SUBTYPES + FEATURE_TYPES, help='The feature type on which to train.')
     parser.add_argument('--binary', default=0, type=bool, help='Whether to train on the binary classification task. If False, then ternary classification is performed.')
     # Optional parameters for Nonlinear classifiers. 
     parser.add_argument('--n-epochs', default=100, type=int, help='The maximum number of epochs to train the Nonlinear classifier.') 
-    parser.add_argument('--lr', default=0.00001, type=float, help='The learning rate for training the Nonlinear classifier.') 
+    parser.add_argument('--lr', default=0.0001, type=float, help='The learning rate for training the Nonlinear classifier.') 
     parser.add_argument('--weight-decay', default=0.01, type=float, help='The L2 regularization penalty to be passed into the Adam optimizer of the Nonlinear classifier.') 
     parser.add_argument('--batch-size', default=16, type=int, help='The size of the batches for Nonlinear classifier training') 
     parser.add_argument('--alpha', default=10, type=int, help='The early stopping threshold for the Nonlinear classifier.') 
@@ -47,41 +47,44 @@ if __name__ == '__main__':
 
     t1 = time.perf_counter()
 
-    # Load the cleaned-up training and validation datasets.
-    training_dataset, validation_dataset = dataset_load_training_validation(args.feature_type, binary=args.binary, to_numpy=True)
-    # Unpack the numpy arrays stored in the dictionary. 
-    X, y, X_val, y_val = training_dataset['features'], training_dataset['labels'], validation_dataset['features'], validation_dataset['labels']
+    feature_types = FEATURE_SUBTYPES + FEATURE_TYPES if args.feature_type is None else args.feature_type
 
-    model_class = getattr(args, 'model-class') # Extract the specified model class.
-    params = read_params(args, model_class=model_class) # Read in model parameters from the command-line arguments.
-    if model_class == 'nonlinear':
-        params.update({'n_classes':3 if not args.binary else 2})
-        params.update({'input_dim':X.shape[-1]}) # Make sure input dimensions are included. 
-        model = GeneralClassifier(model_class=Nonlinear, params=params)
-        model.fit(X, y, X_val=X_val, y_val=y_val)
-    elif model_class == 'logistic':
-        model = GeneralClassifier(model_class=LogisticRegression, params=params)
-        model.fit(X, y)
-    
-    results = evaluate(model, X, y, X_val, y_val)
-    # Store the model parameters in the results dictionary. 
-    results.update(params)
-    results['feature_type'] = args.feature_type # Add feature type to the results.
-    results['model_class'] = model_class
-    results['binary'] = args.binary
+    for feature_type in feature_types:
+        # Load the cleaned-up training and validation datasets.
+        training_dataset, validation_dataset = dataset_load_training_validation(feature_type, binary=args.binary, to_numpy=True)
+        # Unpack the numpy arrays stored in the dictionary. 
+        X, y, X_val, y_val = training_dataset['features'], training_dataset['labels'], validation_dataset['features'], validation_dataset['labels']
 
-    print_summary(results) # Print a summary of the training run to the terminal. 
+        model_class = getattr(args, 'model-class') # Extract the specified model class.
+        params = read_params(args, model_class=model_class) # Read in model parameters from the command-line arguments.
+        if model_class == 'nonlinear':
+            params.update({'n_classes':3 if not args.binary else 2})
+            params.update({'input_dim':X.shape[-1]}) # Make sure input dimensions are included. 
+            model = GeneralClassifier(model_class=Nonlinear, params=params)
+            model.fit(X, y, X_val=X_val, y_val=y_val)
+        elif model_class == 'logistic':
+            model = GeneralClassifier(model_class=LogisticRegression, params=params)
+            model.fit(X, y)
+        
+        results = evaluate(model, X, y, X_val, y_val)
+        # Store the model parameters in the results dictionary. 
+        results.update(params)
+        results['feature_type'] = feature_type # Add feature type to the results.
+        results['model_class'] = model_class
+        results['binary'] = args.binary
 
-    task = 'binary' if args.binary else 'ternary'
+        print_summary(results) # Print a summary of the training run to the terminal. 
 
-    result_path = os.path.join(RESULTS_PATH, f'train_{model_class}_{args.feature_type}_{task}.json')
-    print(f'\nWriting results to {result_path}.')
-    save_results_dict(results, result_path)
+        task = 'binary' if args.binary else 'ternary'
 
-    model_path = os.path.join(MODELS_PATH, f'{model_class}_{args.feature_type}_{task}.joblib')
-    print(f'Saving trained model to {model_path}.')
-    model.save(model_path)
-    
-    t2 = time.perf_counter()
-    print(f'\nModel run complete in {np.round(t2 - t1, 2)} seconds.')
+        result_path = os.path.join(RESULTS_PATH, f'train_{model_class}_{feature_type}_{task}.json')
+        print(f'\nWriting results to {result_path}.')
+        save_results_dict(results, result_path)
+
+        model_path = os.path.join(MODELS_PATH, f'{model_class}_{feature_type}_{task}.joblib')
+        print(f'Saving trained model to {model_path}.')
+        model.save(model_path)
+        
+        t2 = time.perf_counter()
+        print(f'\nModel run complete in {np.round(t2 - t1, 2)} seconds.')
 